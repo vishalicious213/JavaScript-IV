41600be94f9e48f9a7fcba7af3fb0bedcafd7dd4
vishalicious213 <vishalicious213@gmail.com>
2019-08-23 10:25:50 -0400

    Person class created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,15 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attribute){
+        this.name = attribute.name;
+        this.age = attribute.age;
+        this.location = attribute.location;
+    }
+    speak() {
+        {return `${this.name} was removed from the game.`};
+        {return `Hello my name is ${this.name}, I am from ${this.location}.`};
+    }
+}// Person
+
+

0f480e4d682a26a6e09c0fb1bcb0d2831a36d6db
vishalicious213 <vishalicious213@gmail.com>
2019-08-23 10:42:14 -0400

    Instructor sub-class created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,9 +7,24 @@ class Person {
         this.location = attribute.location;
     }
     speak() {
-        {return `${this.name} was removed from the game.`};
         {return `Hello my name is ${this.name}, I am from ${this.location}.`};
     }
 }// Person
+
+
+class Instructor extends Person {
+    constructor(attribute) {
+        super(attribute);
+        this.specialty = attribute.specialty;
+        this.favLanguage = attribute.favLanguage;
+        this.catchPhrase = attribute.catchPhrase;
+    }
+    Demo() {
+        {return `Today we are learning about ${this.specialty}`};
+    }
+    Grade() {
+        {return `student receives a perfect score on ${this.specialty}.`}
+    }
+}// Instructor
 
 
